8976dba521bf75015c2d5dc7eddb2a960873ea8d
Angella Mwangale <amwangal@e1z1r4p34.42.us.org>
2018-04-10 19:07:29 -0700

    changed order

---
+++ 03.OctalHexInitialisation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int var = 45;
+	printf("var = %d\n", var);
+	printf("var = %o\n", var);
+	printf("var = %x\n", var);
+	int var1 = 056;
+	printf("var1 = %d\n", var1);
+	printf("var1 = %o\n", var1);
+	printf("var1 = %x\n", var1);
+	int var2 = 0xa;
+	printf("var2 = %d\n", var2);
+	printf("var2 = %o\n", var2);
+	printf("var2 = %x\n", var2);
+	return 0;
+}
